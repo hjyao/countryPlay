8f1be1f408138e793dfd6855db9544dbd1a2b594
Kai Ramuenke <kai.ramuenke@gmail.com>
2015-01-19 09:47:21 +0530

    switched from mockito-all to mockito-core to avoid transitive dependency issues with hamcrest

--- src/test/java/com/trailblazers/freewheelers/model/ItemValidationTest.java
+++ src/test/java/com/trailblazers/freewheelers/model/ItemValidationTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import static java.math.BigDecimal.valueOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
 
 
 public class ItemValidationTest {
